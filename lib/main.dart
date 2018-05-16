8a1be3398eb9ebacc90b17d28f0556d674b8e671
xur <xur@2345.com>
2018-05-16 21:39:27 +0800

    add package english_words

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
@@ -15,10 +16,25 @@ class MyApp extends StatelessWidget {
             title: new Text("welcome to Flutter"),
           ),
           body: new Center(
-            child: new Text('test'),
+            child: new RandomRords(),
           ),
         )
     );
   }
 }
 
+class RandomRords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new RandomWordsState();
+  }
+
+}
+
+class RandomWordsState extends State<RandomRords>{
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
+  }
+}
