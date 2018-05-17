15dcaaa2a869193d5caeefb692bea1b2ab26c7d2
xur <xur@2345.com>
2018-05-17 14:19:03 +0800

    add new page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
         title: 'Welcome to Flutter',
         theme: new ThemeData(
-         primarySwatch: Colors.red
+            primarySwatch: Colors.red
         ),
         home: new RandomWords()
     );
@@ -23,7 +23,7 @@ class RandomWords extends StatefulWidget {
   }
 }
 
-class RandomWordsState extends State<RandomWords>{
+class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
   final _saved = new Set<WordPair>();
@@ -33,6 +33,9 @@ class RandomWordsState extends State<RandomWords>{
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Startup Name Generator'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
+        ],
       ),
       body: _buildSuggestions(),
     );
@@ -41,10 +44,10 @@ class RandomWordsState extends State<RandomWords>{
   Widget _buildSuggestions() {
     return new ListView.builder(
         padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i){
-          if(i.isOdd) return new Divider();
-          final index = i ~/2;
-          if(index >= _suggestions.length) {
+        itemBuilder: (context, i) {
+          if (i.isOdd) return new Divider();
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10));
           }
           return _buildRow(_suggestions[index]);
@@ -60,17 +63,41 @@ class RandomWordsState extends State<RandomWords>{
       ),
       trailing: new Icon(
         alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red: null,
+        color: alreadySaved ? Colors.red : null,
       ),
       onTap: () {
-        setState((){
-          if(alreadySaved) {
+        setState(() {
+          if (alreadySaved) {
             _saved.remove(suggestion);
           } else {
             _saved.add(suggestion);
           }
         });
       },
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+        new MaterialPageRoute(builder: (context) {
+          final tiles = _saved.map((pair) {
+            return new ListTile(
+              title: new Text(
+                pair.asPascalCase,
+                style: _biggerFont,
+              ),
+            );
+          });
+          final divided = ListTile.divideTiles(context: context, tiles: tiles)
+              .toList();
+
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text("Saved Suggestions"),
+            ),
+            body: new ListView(children: divided),
+          );
+        })
     );
   }
 }
